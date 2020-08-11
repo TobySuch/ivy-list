907ffb49197a662eb6c172a44935d97e315f6e47
Toby Such <toby@tobysuch.uk>
2020-08-11 20:23:58 +0100

    Change username to email as unique key

--- backend/ivy_list/users/models.py
+++ backend/ivy_list/users/models.py
@@ -2,5 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    # See https://stackoverflow.com/a/43209322
+    USERNAME_FIELD = 'email'
+    email = models.EmailField('email address', unique=True) # changes email to unique and blank to false
+    REQUIRED_FIELDS = [] # removes email from REQUIRED_FIELDS
 
