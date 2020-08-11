a92a45e0a2f04f6f01c8dbb3191dd1143b2d3799
Toby Such <toby@tobysuch.uk>
2020-08-11 23:58:14 +0100

    Add ToDoItem model

--- backend/ivy_list/to_do/models.py
+++ backend/ivy_list/to_do/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class ToDoItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120, blank=False)
+    description = models.TextField(blank=True, help_text="Add more details here. You can use markdown to format it!")
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(blank=True, null=True)
+    priority = models.IntegerField(blank=True, null=False)
