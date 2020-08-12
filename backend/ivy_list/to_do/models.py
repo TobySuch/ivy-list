456e31d537882cb6ca68d9c502400bd412ac647c
Toby Such <toby@tobysuch.uk>
2020-08-12 02:53:13 +0100

    Add related name to ToDoItem model

--- backend/ivy_list/to_do/models.py
+++ backend/ivy_list/to_do/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class ToDoItem(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="todo_items", on_delete=models.CASCADE)
     title = models.CharField(max_length=120, blank=False)
     description = models.TextField(blank=True, help_text="Add more details here. You can use markdown to format it!")
     created_at = models.DateTimeField(auto_now_add=True)
