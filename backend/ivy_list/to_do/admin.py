0a97ea7ed00d554c90d1835fc36a8a7ad057fdde
Toby Such <toby@tobysuch.uk>
2020-08-12 13:31:17 +0100

    Add ToDoList model

--- backend/ivy_list/to_do/admin.py
+++ backend/ivy_list/to_do/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import ToDoItem
+from .models import ToDoList, ToDoItem
 
+admin.site.register(ToDoList)
 admin.site.register(ToDoItem)
--- backend/ivy_list/to_do/models.py
+++ backend/ivy_list/to_do/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.conf import settings
 import datetime
+from django.contrib.auth import get_user_model
+
+class ToDoList(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="todo_lists", on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.user.email} ({self.date.isoformat()})"
 
 class ToDoItem(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="todo_items", on_delete=models.CASCADE)
+    todo_list = models.ForeignKey(ToDoList, related_name="todo_items", on_delete=models.CASCADE)
     title = models.CharField(max_length=120, blank=False)
     description = models.TextField(blank=True, help_text="Add more details here. You can use markdown to format it!")
     created_at = models.DateTimeField(auto_now_add=True)
