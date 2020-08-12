885833a6c232bee0d9e98f2acd78fb6d7e68b942
Toby Such <toby@tobysuch.uk>
2020-08-12 03:46:16 +0100

    Change completed_at to read only

--- backend/ivy_list/to_do/serializers.py
+++ backend/ivy_list/to_do/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import ToDoItem
 
 class ToDoItemSerializer(serializers.ModelSerializer):
+    completed_at = serializers.ReadOnlyField()
+    
     class Meta:
         model = ToDoItem
         fields = ['id', 'title', 'description', 'created_at', 'completed_at', 'priority']
