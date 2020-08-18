cfc160aa4afdeed35a93bcf84df26f6aaf60eab2
Toby Such <toby@tobysuch.uk>
2020-08-18 22:38:15 +0100

    Add cursors to buttons

--- frontend/src/components/ToDoItem.vue
+++ frontend/src/components/ToDoItem.vue
@@ -3,29 +3,29 @@
     <div class="row">
       <!-- Hamburger menu to reorder to do items -->
       <div class="col-1">
-        <h3><font-awesome-icon icon="bars" fixed-width/></h3>
+        <h3><font-awesome-icon icon="bars" fixed-width class="grab_cursor"/></h3>
       </div>
 
       <!-- Title of to do item -->
       <div class="col">
         <h3 class="title">
           {{ item.title }}
-          <font-awesome-icon icon="caret-down" fixed-width v-show="!item.description_visible" v-on:click="showDescription(item);"/>
-          <font-awesome-icon icon="caret-up" fixed-width v-show="item.description_visible" v-on:click="hideDescription(item);"/>
+          <font-awesome-icon icon="caret-down" fixed-width v-show="!item.description_visible" v-on:click="showDescription(item);" class="point_cursor"/>
+          <font-awesome-icon icon="caret-up" fixed-width v-show="item.description_visible" v-on:click="hideDescription(item);" class="point_cursor"/>
         </h3>
       </div>
 
       <!-- Buttons at the end of the item -->
       <div class="col-auto align-self-end">
         <h3>
-          <font-awesome-icon icon="check" fixed-width class="ml-2" v-if="item.completed_at === null"/> 
-          <font-awesome-icon icon="times" fixed-width class="ml-2" v-if="item.completed_at !== null"/> 
-          <font-awesome-icon icon="pencil-alt" fixed-width class="ml-2"/> 
-          <font-awesome-icon icon="trash-alt" fixed-width class="ml-2"/>
+          <font-awesome-icon icon="check" fixed-width class="ml-2 point_cursor" v-if="item.completed_at === null"/> 
+          <font-awesome-icon icon="times" fixed-width class="ml-2 point_cursor" v-if="item.completed_at !== null"/> 
+          <font-awesome-icon icon="pencil-alt" fixed-width class="ml-2 point_cursor"/> 
+          <font-awesome-icon icon="trash-alt" fixed-width class="ml-2 point_cursor"/>
         </h3>
       </div>
-
     </div>
+
     <!-- To do item description -->
     <div class="row">
       <div class="col-1"></div>
@@ -62,6 +62,14 @@ export default {
 
   .completed{
     background-color: lightgreen
+  }
+
+  .grab_cursor {
+    cursor: -webkit-grab; cursor: grab;
+  }
+
+  .point_cursor {
+    cursor: pointer;
   }
 
   .description {
