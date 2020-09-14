51bd3b540263dc7c275c2db1ea63d1e5844efebe
Toby Such <toby@tobysuch.uk>
2020-09-14 15:34:46 +0100

    Add nav bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div id="app" class="text-center container">
-    <h1>Ivy List</h1>
+  <div id="app" class="text-center">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="/">Ivy List</a>
+      <div class="navbar-nav ml-auto">
+        <span class="nav-link point_cursor" @click="redirectToLogin">Log In</span>
+        <span class="nav-link point_cursor" @click="redirectToRegister">Register</span>
+      </div>
+    </nav>
     <router-view v-on:login="login"></router-view>
   </div>
 </template>
@@ -8,9 +14,20 @@
 <script>
 export default {
   name: 'App',
+  data: function() {
+    return {
+      loggedIn: false
+    }
+  },
   methods: {
     login: function () {
-      this.$router.push("/todo")
+      this.$router.push("/todo");
+    },
+    redirectToLogin: function() {
+      this.$router.push("/login");
+    },
+    redirectToRegister: function() {
+      this.$router.push("/register");
     }
   }
 }
@@ -18,7 +35,9 @@ export default {
 
 <style>
 #app {
-  margin-top: 60px;
+  margin: 0px;
+  padding: 0px;
+  /*margin-top: 60px;*/
 }
 
 #title {
