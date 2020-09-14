d411d5b7dca01bf5152fea001863ef1d153ba100
Toby Such <toby@tobysuch.uk>
2020-09-14 22:25:45 +0100

    Fix bug where logging in would freeze up vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <template>
-  <h2>Home</h2>
-
+  <div>
+    <h2>Home</h2>
+    <button class="btn btn-primary" @click="redirectToToDos">View Your To Do's</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  methods: {
+    redirectToToDos: function () {
+      this.$router.push('/todo');
+    }
+  }
 }
 </script>
 
